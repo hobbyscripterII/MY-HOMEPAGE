cab232e5c3a82a952b810a43660e485c11d9f662
이주영 <wruomma2@naver.com>
2024-11-24 16:48:55 +0900

    update: - 불필요 img 제거 - 랜덤 이미지 표출 기능 삭제 - css 정리

--- src/main/java/com/project/homepage/home/HomeController.java
+++ src/main/java/com/project/homepage/home/HomeController.java
@@ -6,14 +6,10 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import javax.xml.parsers.ParserConfigurationException;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,7 +17,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.xml.sax.SAXException;
 
 import com.project.homepage.cmmn.Const;
-import com.project.homepage.cmmn.util.RSSParseUtil;
 
 @Controller
 public class HomeController {
@@ -29,21 +24,15 @@ public class HomeController {
 	public String home(@RequestParam Map<String, Object> requestMap, Model model) throws ParserConfigurationException, SAXException, IOException {
 		// 날짜 및 일수 계산
 		Map<String, Object> daysGet = new HashMap<String, Object>();
-		LocalDateTime CURRENT_      = LocalDateTime.now();
-		String CURRENT 		        = DateTimeFormatter.ofPattern("yyyy-MM-dd").format(CURRENT_);
-		LocalDate DEPOLY_           = LocalDate.of(2024, 9, 24);
-		String DEPOLY               = DateTimeFormatter.ofPattern("yyyy-MM-dd").format(DEPOLY_);
-		long CNT				    = ChronoUnit.DAYS.between(DEPOLY_, CURRENT_);
+		String CURRENT 		        = DateTimeFormatter.ofPattern("yyyy-MM-dd").format(LocalDateTime.now());
+		String DEPOLY               = DateTimeFormatter.ofPattern("yyyy-MM-dd").format(LocalDate.of(2024, 9, 24));
+		long CNT				    = ChronoUnit.DAYS.between(LocalDate.of(2024, 9, 24), LocalDateTime.now());
 		
-		daysGet.put("CNT"    , CNT);
 		daysGet.put("CURRENT", CURRENT);
 		daysGet.put("DEPOLY" , DEPOLY);
-		
-		// 랜덤 이미지 표출
-		int num = (int) (Math.random() * 7) + 1;
+		daysGet.put("CNT"    , CNT);
 		
         model.addAttribute(Const.DATE, daysGet);
-        model.addAttribute(Const.NUM , num);
 		
 		return "home";
 	}
