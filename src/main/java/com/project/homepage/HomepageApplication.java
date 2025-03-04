88c74c26b44e53fae300788685d0d575ebff3f1e
hobbyscripterII <wruomma2@naver.com>
2025-03-04 22:46:11 +0900

    [2025-03-04] UPDATE - Spring Eureka Client 적용

--- src/main/java/com/project/homepage/HomepageApplication.java
+++ src/main/java/com/project/homepage/HomepageApplication.java
@@ -2,8 +2,10 @@ package com.project.homepage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableDiscoveryClient
 @EnableScheduling
 @SpringBootApplication
 public class HomepageApplication {
